86e6f5cb4834ba027dfde9733419c37d6bf81c31
qipa-dong <1423883450@qq.com>
2018-11-21 11:09:40 +0800

    调整界面与显示

--- 磁盘编辑工具/Form1.Designer.cs
+++ 磁盘编辑工具/Form1.Designer.cs
@@ -32,25 +32,13 @@
 			this.button1 = new System.Windows.Forms.Button();
 			this.progressBar1 = new System.Windows.Forms.ProgressBar();
 			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.label3 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
 			this.label5 = new System.Windows.Forms.Label();
 			this.textBox4 = new System.Windows.Forms.TextBox();
 			this.button2 = new System.Windows.Forms.Button();
 			this.button3 = new System.Windows.Forms.Button();
 			this.label6 = new System.Windows.Forms.Label();
-			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-			this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
-			this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
 			this.label7 = new System.Windows.Forms.Label();
 			this.comboBox2 = new System.Windows.Forms.ComboBox();
-			this.comboBox3 = new System.Windows.Forms.ComboBox();
-			this.comboBox4 = new System.Windows.Forms.ComboBox();
-			this.comboBox5 = new System.Windows.Forms.ComboBox();
-			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// comboBox1
@@ -58,7 +46,7 @@
 			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.comboBox1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
 			this.comboBox1.FormattingEnabled = true;
-			this.comboBox1.Location = new System.Drawing.Point(143, 14);
+			this.comboBox1.Location = new System.Drawing.Point(98, 14);
 			this.comboBox1.Name = "comboBox1";
 			this.comboBox1.Size = new System.Drawing.Size(156, 20);
 			this.comboBox1.TabIndex = 0;
@@ -66,7 +54,7 @@
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(318, 11);
+			this.button1.Location = new System.Drawing.Point(273, 11);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(54, 24);
 			this.button1.TabIndex = 1;
@@ -76,9 +64,9 @@
 			// 
 			// progressBar1
 			// 
-			this.progressBar1.Location = new System.Drawing.Point(26, 238);
+			this.progressBar1.Location = new System.Drawing.Point(26, 123);
 			this.progressBar1.Name = "progressBar1";
-			this.progressBar1.Size = new System.Drawing.Size(273, 32);
+			this.progressBar1.Size = new System.Drawing.Size(228, 32);
 			this.progressBar1.TabIndex = 2;
 			// 
 			// label1
@@ -90,36 +78,6 @@
 			this.label1.Size = new System.Drawing.Size(29, 12);
 			this.label1.TabIndex = 4;
 			this.label1.Text = "磁盘";
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-			this.label2.Location = new System.Drawing.Point(24, 122);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(77, 12);
-			this.label2.TabIndex = 5;
-			this.label2.Text = "磁盘写入地址";
-			// 
-			// label3
-			// 
-			this.label3.AutoSize = true;
-			this.label3.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-			this.label3.Location = new System.Drawing.Point(24, 158);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(77, 12);
-			this.label3.TabIndex = 6;
-			this.label3.Text = "写入数据长度";
-			// 
-			// label4
-			// 
-			this.label4.AutoSize = true;
-			this.label4.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-			this.label4.Location = new System.Drawing.Point(24, 194);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(77, 12);
-			this.label4.TabIndex = 8;
-			this.label4.Text = "文件开始地址";
 			// 
 			// label5
 			// 
@@ -134,7 +92,7 @@
 			// textBox4
 			// 
 			this.textBox4.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-			this.textBox4.Location = new System.Drawing.Point(143, 83);
+			this.textBox4.Location = new System.Drawing.Point(98, 83);
 			this.textBox4.Name = "textBox4";
 			this.textBox4.ReadOnly = true;
 			this.textBox4.Size = new System.Drawing.Size(156, 21);
@@ -142,7 +100,7 @@
 			// 
 			// button2
 			// 
-			this.button2.Location = new System.Drawing.Point(318, 81);
+			this.button2.Location = new System.Drawing.Point(273, 81);
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(54, 23);
 			this.button2.TabIndex = 12;
@@ -152,7 +110,7 @@
 			// 
 			// button3
 			// 
-			this.button3.Location = new System.Drawing.Point(304, 238);
+			this.button3.Location = new System.Drawing.Point(259, 123);
 			this.button3.Name = "button3";
 			this.button3.Size = new System.Drawing.Size(68, 32);
 			this.button3.TabIndex = 13;
@@ -163,32 +121,11 @@
 			// label6
 			// 
 			this.label6.AutoSize = true;
-			this.label6.Location = new System.Drawing.Point(24, 294);
+			this.label6.Location = new System.Drawing.Point(24, 175);
 			this.label6.Name = "label6";
 			this.label6.Size = new System.Drawing.Size(53, 12);
 			this.label6.TabIndex = 14;
 			this.label6.Text = "磁盘信息\r\n";
-			// 
-			// numericUpDown1
-			// 
-			this.numericUpDown1.Location = new System.Drawing.Point(143, 120);
-			this.numericUpDown1.Name = "numericUpDown1";
-			this.numericUpDown1.Size = new System.Drawing.Size(156, 21);
-			this.numericUpDown1.TabIndex = 15;
-			// 
-			// numericUpDown2
-			// 
-			this.numericUpDown2.Location = new System.Drawing.Point(143, 156);
-			this.numericUpDown2.Name = "numericUpDown2";
-			this.numericUpDown2.Size = new System.Drawing.Size(156, 21);
-			this.numericUpDown2.TabIndex = 16;
-			// 
-			// numericUpDown3
-			// 
-			this.numericUpDown3.Location = new System.Drawing.Point(143, 192);
-			this.numericUpDown3.Name = "numericUpDown3";
-			this.numericUpDown3.Size = new System.Drawing.Size(156, 21);
-			this.numericUpDown3.TabIndex = 17;
 			// 
 			// label7
 			// 
@@ -209,71 +146,23 @@
             "读取",
             "写入",
             "擦除"});
-			this.comboBox2.Location = new System.Drawing.Point(143, 51);
+			this.comboBox2.Location = new System.Drawing.Point(98, 51);
 			this.comboBox2.Name = "comboBox2";
 			this.comboBox2.Size = new System.Drawing.Size(156, 20);
 			this.comboBox2.TabIndex = 19;
-			// 
-			// comboBox3
-			// 
-			this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.comboBox3.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-			this.comboBox3.FormattingEnabled = true;
-			this.comboBox3.Items.AddRange(new object[] {
-            "HEX",
-            "DEC"});
-			this.comboBox3.Location = new System.Drawing.Point(318, 119);
-			this.comboBox3.Name = "comboBox3";
-			this.comboBox3.Size = new System.Drawing.Size(54, 20);
-			this.comboBox3.TabIndex = 20;
-			// 
-			// comboBox4
-			// 
-			this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.comboBox4.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-			this.comboBox4.FormattingEnabled = true;
-			this.comboBox4.Items.AddRange(new object[] {
-            "HEX",
-            "DEC"});
-			this.comboBox4.Location = new System.Drawing.Point(318, 155);
-			this.comboBox4.Name = "comboBox4";
-			this.comboBox4.Size = new System.Drawing.Size(54, 20);
-			this.comboBox4.TabIndex = 21;
-			// 
-			// comboBox5
-			// 
-			this.comboBox5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.comboBox5.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-			this.comboBox5.FormattingEnabled = true;
-			this.comboBox5.Items.AddRange(new object[] {
-            "HEX",
-            "DEC"});
-			this.comboBox5.Location = new System.Drawing.Point(318, 191);
-			this.comboBox5.Name = "comboBox5";
-			this.comboBox5.Size = new System.Drawing.Size(54, 20);
-			this.comboBox5.TabIndex = 22;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(405, 447);
-			this.Controls.Add(this.comboBox5);
-			this.Controls.Add(this.comboBox4);
-			this.Controls.Add(this.comboBox3);
+			this.ClientSize = new System.Drawing.Size(348, 313);
 			this.Controls.Add(this.comboBox2);
 			this.Controls.Add(this.label7);
-			this.Controls.Add(this.numericUpDown3);
-			this.Controls.Add(this.numericUpDown2);
-			this.Controls.Add(this.numericUpDown1);
 			this.Controls.Add(this.label6);
 			this.Controls.Add(this.button3);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.textBox4);
 			this.Controls.Add(this.label5);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.progressBar1);
 			this.Controls.Add(this.button1);
@@ -281,9 +170,6 @@
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.Name = "Form1";
 			this.Text = "Form1";
-			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -295,22 +181,13 @@
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.ProgressBar progressBar1;
 		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.Label label4;
 		private System.Windows.Forms.Label label5;
 		private System.Windows.Forms.TextBox textBox4;
 		private System.Windows.Forms.Button button2;
 		private System.Windows.Forms.Button button3;
 		private System.Windows.Forms.Label label6;
-		private System.Windows.Forms.NumericUpDown numericUpDown1;
-		private System.Windows.Forms.NumericUpDown numericUpDown2;
-		private System.Windows.Forms.NumericUpDown numericUpDown3;
 		private System.Windows.Forms.Label label7;
 		private System.Windows.Forms.ComboBox comboBox2;
-		private System.Windows.Forms.ComboBox comboBox3;
-		private System.Windows.Forms.ComboBox comboBox4;
-		private System.Windows.Forms.ComboBox comboBox5;
 	}
 }
 
--- 磁盘编辑工具/Form1.cs
+++ 磁盘编辑工具/Form1.cs
@@ -46,27 +46,14 @@ namespace 磁盘编辑工具
 
 		private void Get_info()
 		{
-			long lsum = 0,ldr;
+			long lsum = 0,ldr =0;
 			StringBuilder mStringBuilder = new StringBuilder();
 			comboBox1.Items.Clear();
 			label6.Text = "";
 			foreach (DriveInfo drive in DriveInfo.GetDrives())
 			{
-				//判断是否是固定磁盘  
-				if (drive.DriveType == DriveType.Fixed)
-				{
-					lsum = drive.TotalSize / 1024 /1024;//MB,磁盘总大小
-					ldr = drive.TotalFreeSpace / 1024 /1024;//剩余大小
-					label6.Text += "固定磁盘：";
-					label6.Text += drive.Name + ": 总空间=" + lsum.ToString("n") + " MB" + " 剩余空间= " + ldr.ToString("n") + " MB" + "\r\n";
-				}
-				else if (drive.DriveType == DriveType.Removable)
-				{
-					lsum = drive.TotalSize / 1024 / 1024;//MB,磁盘总大小
-					ldr = drive.TotalFreeSpace / 1024 / 1024;//剩余大小
-					label6.Text += "移动磁盘：";
-					label6.Text += drive.Name + ": 总空间=" + lsum.ToString("n") + " 剩余空间= " + ldr.ToString("n") + " MB" + "\r\n";
-				}
+				lsum = drive.TotalSize / 1024 / 1024;//MB,磁盘总大小
+				ldr = drive.TotalFreeSpace / 1024 / 1024;//剩余大小
 
 				if(lsum <= 0x2000)
 				{
@@ -74,6 +61,18 @@ namespace 磁盘编辑工具
 						comboBox1.Items.Add(drive.Name + drive.VolumeLabel);
 					else
 						comboBox1.Items.Add(drive.Name + "未知");
+
+					//判断是否是固定磁盘  
+					if (drive.DriveType == DriveType.Fixed)
+					{
+						label6.Text += "固定磁盘：";
+					}
+					else if (drive.DriveType == DriveType.Removable)
+					{
+						label6.Text += "移动磁盘：";
+					}
+
+					label6.Text += drive.Name + ": 总空间=" + lsum.ToString("n") + " MB" + " 剩余空间= " + ldr.ToString("n") + " MB" + "\r\n";
 				}
 			}
 			if(comboBox1.Items.Count > 0)
